60e0ecf54fefd03e816b332107ec8520182b1425
xlow <lowxf7@gmail.com>
2025-01-14 17:54:39 +0800

    added pawn move, capture, promotion, enpassant logic to pawnGeneration

--- pawnGeneration.cpp
+++ pawnGeneration.cpp
@@ -29,4 +29,34 @@ std::vector<Move> generatePawnMoves(
     doublePush = (doublePush << 8) & emptySquares;
 
     // Captures
-    capturesLeft = (pawns << 7) & opponentPieces & ~
+    capturesLeft = (pawns << 7) & opponentPieces & ~0x8080808080808080; // prevents wrapping
+    capturesRight = (pawns << 9) & opponentPieces & ~0x0101010101010101; // e.g left captures to right edge
+    promotions |= (capturesLeft | capturesRight) & 0xFF00000000000000; // promotion captures
+    capturesLeft &= ~0xFF00000000000000; // non promotion captures
+    capturesRight &= ~0xFF00000000000000;
+
+    // En passant
+    capturesLeft |= (pawns << 7) & enPassantTarget & ~0x8080808080808080;
+    capturesRight |= (pawns << 9) & enPassantTarget & ~0x0101010101010101;
+  }
+  else {
+    // Single push
+    singlePush = (pawns >> 8) & emptySquares;
+    promotions = singlePush & 0x00000000000000FF; // promotion squares
+    singlePush &= ~0x00000000000000FF; // non promotion squares
+
+    // Double push
+    doublePush = ((pawns & 0x00FF000000000000) >> 8) & emptySquares;
+    doublePush = (doublePush >> 8) & emptySquares;
+
+    // Captures
+    capturesLeft = (pawns >> 9) & opponentPieces & ~0x8080808080808080; // prevents wrapping
+    capturesRight = (pawns >> 7) & opponentPieces & ~0x0101010101010101;
+    promotions |= (capturesLeft | capturesRight) & 0x00000000000000FF; // promotion captures
+    capturesLeft &= ~0x00000000000000FF;
+    capturesRight &= ~0x00000000000000FF;
+
+    // En passant
+    capturesLeft |= (pawns >> 9) & enPassantTarget & ~0x8080808080808080;
+    capturesRight |= (pawns >> 7) & enPassantTarget & ~0x0101010101010101;
+  }
