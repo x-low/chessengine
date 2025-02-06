799690939af55ac54636c9de415628b75e62515a
xlow <lowxf7@gmail.com>
2025-02-07 04:07:44 +0800

    changed to constexpr in constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -2,25 +2,25 @@
 
 #include <cstdint>
 
-const uint64_t  FILE_A = 0x0101010101010101;
-const uint64_t  FILE_B = 0x0202020202020202;
-const uint64_t  FILE_C = 0x0404040404040404;
-const uint64_t  FILE_D = 0x0808080808080808;
-const uint64_t  FILE_E = 0x1010101010101010;
-const uint64_t  FILE_F = 0x2020202020202020;
-const uint64_t  FILE_G = 0x4040404040404040;
-const uint64_t  FILE_H = 0x8080808080808080;
+constexpr uint64_t  FILE_A = 0x0101010101010101;
+constexpr uint64_t  FILE_B = 0x0202020202020202;
+constexpr uint64_t  FILE_C = 0x0404040404040404;
+constexpr uint64_t  FILE_D = 0x0808080808080808;
+constexpr uint64_t  FILE_E = 0x1010101010101010;
+constexpr uint64_t  FILE_F = 0x2020202020202020;
+constexpr uint64_t  FILE_G = 0x4040404040404040;
+constexpr uint64_t  FILE_H = 0x8080808080808080;
 
-const uint64_t  RANK_1 = 0x00000000000000FF;
-const uint64_t  RANK_2 = 0x000000000000FF00;
-const uint64_t  RANK_3 = 0x0000000000FF0000;
-const uint64_t  RANK_4 = 0x00000000FF000000;
-const uint64_t  RANK_5 = 0x000000FF00000000;
-const uint64_t  RANK_6 = 0x0000FF0000000000;
-const uint64_t  RANK_7 = 0x00FF000000000000;
-const uint64_t  RANK_8 = 0xFF00000000000000;
+constexpr uint64_t  RANK_1 = 0x00000000000000FF;
+constexpr uint64_t  RANK_2 = 0x000000000000FF00;
+constexpr uint64_t  RANK_3 = 0x0000000000FF0000;
+constexpr uint64_t  RANK_4 = 0x00000000FF000000;
+constexpr uint64_t  RANK_5 = 0x000000FF00000000;
+constexpr uint64_t  RANK_6 = 0x0000FF0000000000;
+constexpr uint64_t  RANK_7 = 0x00FF000000000000;
+constexpr uint64_t  RANK_8 = 0xFF00000000000000;
 
-const int       BOARD_SIZE = 64;
+constexpr int       BOARD_SIZE = 64;
 
 enum Square {
     A1 = 0, B1, C1, D1, E1, F1, G1, H1,
